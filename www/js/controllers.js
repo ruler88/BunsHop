63f095332345c05115622497458b84eaa138bd4d
ruler88 <helloworld0424@gmail.com>
2014-09-09 23:34:34 -0700

    added cute icons

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,12 @@
 angular.module('starter.controllers', [])
 
 	.controller('MapController', function ($scope, $ionicLoading) {
+
+		var bunsIcons = {
+			kai: 'img/kai_buns.png',
+			sarah: 'img/sarah_buns.png'
+		};
+
 		$scope.mapCreated = function (map) {
 			$scope.map = map;
 		};
@@ -19,6 +25,14 @@ angular.module('starter.controllers', [])
 			navigator.geolocation.getCurrentPosition(function (pos) {
 				console.log('Got pos', pos);
 				$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+				var marker = new google.maps.Marker({
+					position: $scope.map.getCenter(),
+					map: $scope.map,
+					title: 'Click to zoom',
+					animation: google.maps.Animation.BOUNCE,
+					icon: bunsIcons.kai
+				});
+
 				$scope.loading.hide();
 			}, function (error) {
 				alert('Unable to get location: ' + error.message);
