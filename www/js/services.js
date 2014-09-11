4aea06978c9b928b0e1c5a92c801ae7a8269bb88
ruler88 <helloworld0424@gmail.com>
2014-09-10 22:44:56 -0700

    abstract marker update function

--- www/js/services.js
+++ www/js/services.js
@@ -3,13 +3,23 @@ var bunsIcons = {
 	kai: 'img/kai_buns.gif',
 	sarah: 'img/sarah_buns.gif'
 };
-
+var marker;
 
 angular.module('map.services', [])
 
 	.service('MapService', function() {
-
-		var marker;
+		var updateMarkerLocation = function($scope, googPosition) {
+			if (!marker) {
+				marker = new google.maps.Marker({
+					position: new google.maps.LatLng(googPosition),
+					map: $scope.map,
+					title: 'Click to zoom',
+					animation: google.maps.Animation.BOUNCE,
+					icon: bunsIcons.sarah
+				});
+			}
+			marker.setPosition(googPosition);
+		}
 
 		this.centerOnMe = function ($scope, $ionicLoading) {
 			console.log("Centering");
@@ -26,19 +36,7 @@ angular.module('map.services', [])
 				console.log('Got pos', pos);
 				var googPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
 				$scope.map.setCenter(googPosition);
-
-				if (!marker) {
-					marker = new google.maps.Marker({
-						position: new google.maps.LatLng(googPosition),
-						map: $scope.map,
-						title: 'Click to zoom',
-						animation: google.maps.Animation.BOUNCE,
-						icon: bunsIcons.sarah
-					});
-				}
-				marker.setPosition(googPosition);
-
-
+				updateMarkerLocation($scope, googPosition);
 				$ionicLoading.hide();
 			}, function (error) {
 				alert('Unable to get location: ' + error.message);
@@ -54,14 +52,13 @@ angular.module('map.services', [])
 			}
 
 			// onError Callback receives a PositionError object
-			//
 			function onError(error) {
+				//TODO send msg about no loc
 				alert('code: '    + error.code    + '\n' +
 					'message: ' + error.message + '\n');
 			}
 
 			// Options: throw an error if no update is received every 30 seconds.
-			//
-			var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+			navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
 		}
 	});
