1b8368371bc527617a8538dd3bae79e1b3317075
ruler88 <helloworld0424@gmail.com>
2014-09-14 00:33:36 -0700

    using fb signin in place of goog

--- www/keys.js
+++ www/keys.js
@@ -1,4 +1,7 @@
 var clientId_goog_auth = '420168715514-e2a5namt763208omlq4ets71urodrao5.apps.googleusercontent.com';
-var client_secret_goog_auth = 'eUU_8QaLgfUyDVnnTUmkvkNO';
+var client_secret_goog_auth = 'l34Bh0X_kc4kPIbMUKy1DuIx';
 
 var apiKey_goog_client = 'AIzaSyBXnO6zhNaEekfuJz4tbICKYDuZ3yJmNEs';
+
+var fb_app_id = '576012412528125';
+var fb_app_secret = '393100de393472f28c9a30574e8f8d26';
