5b3a9fa31315bcff5b8743476387dfed2bcefbc9
ruler88 <helloworld0424@gmail.com>
2014-09-28 20:45:31 -0700

    adding in mobile device in notifyTest

--- notifyTest.js
+++ notifyTest.js
@@ -9,6 +9,7 @@ message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before
 
 // At least one reg id required
 registrationIds.push('APA91bF__pSyj9ORTIHxiBmpBZJKE0cwM3shyVCOxBjNSlZ6MSAre8taFgbLr_cNwLYQDHyoA5LD6leM1_a8XODKghqGCmLSbH_akejKg3eHvd4QLeETycJCHHxgegFqbZV-F4KfkoQARyZbBlzfh4ed0wDFdRlqX0b31g_CozE-Vpf1r_3K7C4');
+registrationIds.push('APA91bHCSM1tVQ5C9DsApCboYLwXMqUd7ybPGPlsa6E6zGDw9ss0HN7ozAO9_XG05-CEx5pHBYa9XDntYdSOJw3FtXGZmXu_ogSRIVYcKU4T9nRztsq8y-159q8Aad-y9wbOEKoa81kO9vpHQebylLF4TzqeplUXsKWvmAfXJE22ap0xGW8TfQY');
 
 /**
 * Parameters: message-literal, registrationIds-array, No. of retries, callback-function
